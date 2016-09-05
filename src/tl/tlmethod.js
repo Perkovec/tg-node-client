0712fac37fc3c408e2f53a62abe1ba9b777a62fd
Perkovec <perkovec24@gmail.com>
2016-09-05 23:53:57 +0300

    TLMethod

---
+++ src/tl/tlmethod.js
@@ -0,0 +1,10 @@
+class TLMethod {
+  constructor(data) {
+    this.id = data.id;
+    this.type = data.type;
+    this.method = data.method;
+    this.params = data.params;
+  }
+}
+
+module.exports = TLMethod;
