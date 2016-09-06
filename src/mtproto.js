2d7d4bbc16f5bbdb351877e117f2c19f7fb298f5
Perkovec <perkovec24@gmail.com>
2016-09-07 00:50:25 +0300

    generate auth key hash

--- src/mtproto.js
+++ src/mtproto.js
@@ -1,5 +1,6 @@
 const {Socket} = require('net');
 const {randomBytes} = require('crypto');
+const sha1 = require('sha1');
 
 const api_id = 44026;
 const api_hash = "e864dc7132da460ffdc67c1b7cf66e1e";
@@ -14,7 +15,7 @@ class MTProto {
       this.session_id = buffer;
     });
     this.auth_key = auth_key;
-    this.auth_key_id = ''; // TODO
+    this.auth_key_id = this.auth_key ? sha1(this.auth_key).slice(-8) : null;
     this.sock.setTimeout(5);
     this.MAX_RETRY = 5;
     this.AUTH_MAX_RETRY = 5;
